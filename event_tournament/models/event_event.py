d8ac4af2d52504e62ae514f6be10e17562725ff9
Simone <daemo00@gmail.com>
2019-07-28 20:23:57 +0200

    models and constraints

---
+++ event_tournament/models/event_event.py
@@ -0,0 +1,13 @@
+#  Copyright 2019 Simone Rubino <daemo00@gmail.com>
+#  License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class EventEvent(models.Model):
+    _inherit = 'event.event'
+
+    tournament_ids = fields.One2many(
+        comodel_name='event.tournament',
+        inverse_name='event_id',
+        string="Tournament")
