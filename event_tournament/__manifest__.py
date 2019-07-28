3cfde8f1bd04cec71bf485a30ec6b363ff2e646b
Simone <daemo00@gmail.com>
2019-07-28 20:23:57 +0200

    models and constraints

---
+++ event_tournament/__manifest__.py
@@ -0,0 +1,20 @@
+#  Copyright 2019 Simone Rubino <daemo00@gmail.com>
+#  License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': "Event tournament",
+    'summary': "Implement tournaments in Odoo events",
+    'version': "12.0.1.0.0",
+    'license': "AGPL-3",
+    'depends': [
+        'event',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/event_event_view.xml',
+        'views/event_registration_view.xml',
+        'views/event_tournament_view.xml',
+        'views/event_tournament_match_view.xml',
+        'views/event_tournament_team_view.xml',
+    ],
+}
---
+++ event_tournament/models/__init__.py
@@ -0,0 +1,7 @@
+#  License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import event_event
+from . import event_registration
+from . import event_tournament
+from . import event_tournament_match
+from . import event_tournament_team
