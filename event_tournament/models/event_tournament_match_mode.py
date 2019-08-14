ecd07855757196dd66408adbb0bf6b1f664ff2ea
Simone <daemo00@gmail.com>
2019-08-14 23:53:07 +0200

    better points computation for matches

--- event_tournament/models/event_tournament_match_mode.py
+++ event_tournament/models/event_tournament_match_mode.py
@@ -1,5 +1,6 @@
 #  Copyright 2019 Simone Rubino <daemo00@gmail.com>
 #  License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from collections import Counter
 
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
@@ -27,18 +28,25 @@ class EventTournamentMatchMode(models.Model):
         inverse_name='mode_id')
 
     @api.multi
-    def get_points(self, sets_won, sets_lost):
+    def get_points(self, match):
         self.ensure_one()
-        for res in self.result_ids:
-            if res.sets_won == sets_won and res.sets_lost == sets_lost:
-                points = res.points_win
-                break
-            if res.sets_won == sets_lost and res.sets_lost == sets_won:
-                points = res.points_lose
-                break
-        else:
-            raise UserError(_("Result {sets_won} - {sets_lost} not expected")
-                            .format(
-                                sets_won=sets_won,
-                                sets_lost=sets_lost))
-        return points
+        sets_played, team_sets_won = match.get_sets_info()
+        team_points = dict.fromkeys(match.team_ids, 0)
+        for team, sets_won in team_sets_won.items():
+            sets_lost = sets_played - sets_won
+            for res in self.result_ids:
+                if res.sets_won == sets_won and res.sets_lost == sets_lost:
+                    team_points[team] = res.points_win
+                    break
+                if res.sets_won == sets_lost and res.sets_lost == sets_won:
+                    team_points[team] = res.points_lose
+                    break
+            else:
+                raise UserError(
+                    _("Match {match_name}:\n"
+                      "Result {sets_won} - {sets_lost} not expected")
+                    .format(
+                        match_name=match.display_name,
+                        sets_won=sets_won,
+                        sets_lost=sets_lost))
+        return team_points
