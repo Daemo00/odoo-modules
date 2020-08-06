2fe21f66358e807c797f8c43292d24ce1de48579
Simone Rubino <daemo00@gmail.com>
2020-08-06 23:53:46 +0200

    [ADD] event_tournament
    * Initial commit, samples from https://github.com/OCA/maintainer-quality-tools/tree/master/sample_files
    
    * models and constraints
    
    * match button_win
    
    * match with sets
    
    * match perfection
    
    * tournament, team and match fixes
    
    * fix team constrains
    
    * tournament rules
    
    * court
    
    * court constrains and various improvements
    
    * matches generation
    
    * check components in other courts
    
    * match computed fields
    
    * warm_up
    
    * try smart match assignation
    
    * smart match assignation working
    
    * randomize option
    
    * reset option
    
    * resetting only undone matches
    
    * better contemporary domain
    
    * better filter for team and match
    
    * fix tournament started state
    
    * team points ratio
    
    * refactoring team components constrains, tournament search view
    
    * computing team points
    
    * added tournament mode
    
    * added tournament mode points computation
    
    * data for match modes
    
    * refactor tournament actions
    
    * max start for team generation is last match's end
    
    * better data
    
    * time constraint on court
    
    * better points computation for matches
    
    * manage tied match with no winner
    
    * ties not allowed in sets
    
    * recompute points button in tournament
    
    * done matches can't be edited
    
    * matches schedule report
    
    * kanban view for matches
    
    * fixes by creating new tournament
    
    * posticipate end to allow scheduling a match after all matches
    
    * open tournament full-screen
    
    * very basic import wizard, no logic
    
    * parsing team lines
    
    * parsing team lines to values for team model
    
    * import succeeds
    
    * import succeeds and is correct
    
    * refactor
    
    * translations
    
    * add mobile
    
    * match schedule report
    
    * avoid playing two matches in a row
    
    * fix translation
    
    * fix sets played, match view
    
    * clean registration name
    
    * [MIG] event_tournament: Migration to 13.0
    
    * [IMP] event_tournament: Estimated match count
    
    * [IMP] event_tournament: Court check in matches generation
    
    * [IMP] event_tournament: Updated it translations
    
    * [IMP] event_tournament: More strict check on tournament name, components field in tournament, more strict check with existing registrations during import, updated it translations
    
    * [IMP] event_tournament: First tests
    
    * [IMP] event_tournament: test event_registration
    
    * [IMP] event_tournament: initial tests for event_tournament
    
    * [IMP] event_tournament: test points computation
    
    * [IMP] event_tournament: fix warning 'event_id' is readonly
    
    * [IMP] event_tournament: test match mode error, begin match generation tests
    
    * [IMP] event_tournament: more match generation tests
    
    * [IMP] event_tournament: clean common tests
    
    * [IMP] event_tournament: reformat winner computation and tests
    
    * [IMP] event_tournament: test team, better data generation for tests
    
    * [IMP] event_tournament: test tournament states
    
    * [IMP] event_tournament: improved generic tests data, tournament tests
    
    * [IMP] event_tournament: refactor common tests
    
    * [IMP] event_tournament: improved team ui and tests
    
    * [IMP] event_tournament: updated translations
    
    * [IMP] event_tournament: gender tests on teams
    
    * [IMP] event_tournament: increased event_tournament coverage
    
    * [IMP] event_tournament: increased event_tournament_team coverage
    
    * pre-commit
    
    * update quality tools configuration

---
+++ event_tournament/tests/test_import_csv_bv4w.py
@@ -0,0 +1,112 @@
+#  Copyright 2019 Simone Rubino <daemo00@gmail.com>
+#  License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+import base64
+import os
+
+from odoo.exceptions import UserError, ValidationError
+from odoo.modules import get_resource_path
+
+from .test_common import TEAM_NBR, TestCommon
+
+
+class TestImportCsvBV4W(TestCommon):
+    def setUp(self):
+        super().setUp()
+        self.wizard_model = self.env["event.tournament.import_csv_bv4w"]
+
+    def get_wizard_for_file(self, file_name):
+        resp_path = get_resource_path(self.test_module, "tests", "data", file_name)
+        with open(resp_path, "rb") as resp_file:
+            wizard = self.wizard_model.create(
+                {
+                    "data": base64.encodebytes(resp_file.read()),
+                    "filename": os.path.basename(resp_file.name),
+                }
+            )
+        return wizard
+
+    def test_import_csv_bv4w_no_tournament(self):
+        """
+        Import the file when no tournament exists,
+        an exception should be raised.
+        """
+        wizard = self.get_wizard_for_file("bv4w_test_1.csv")
+        tournament = self.tournaments.filtered(
+            lambda t: t.name in ["event 0, tournament 0"]
+        )
+        tournament_name = tournament.name
+        tournament.unlink()
+        with self.assertRaises(UserError) as ue:
+            wizard.import_csv_bv4w()
+        self.assertIn(tournament_name, ue.exception.name)
+
+    def test_import_csv_bv4w_teams(self):
+        """
+        Import the file, check that the teams are created.
+        """
+        wizard = self.get_wizard_for_file("bv4w_test_1.csv")
+        tournament = self.tournaments.filtered(
+            lambda t: t.name in ["event 0, tournament 0"]
+        )
+        self.assertEqual(len(tournament.team_ids), TEAM_NBR)
+        wizard.import_csv_bv4w()
+        self.assertEqual(len(tournament.team_ids), 2 + TEAM_NBR)
+
+    def test_import_csv_bv4w_components(self):
+        """
+        Import the file, check that the components are created.
+        """
+        wizard = self.get_wizard_for_file("bv4w_test_1.csv")
+        tournament = self.tournaments.filtered(
+            lambda t: t.name in ["event 0, tournament 0"]
+        )
+        tournament.component_ids.unlink()
+        wizard.import_csv_bv4w()
+        self.assertEqual(len(tournament.component_ids), 4)
+
+    def test_import_csv_bv4w_components_overlap_error(self):
+        """
+        Import the file, check that the components are created
+        and overlapping components from same tournaments raise exception.
+        """
+        wizard = self.get_wizard_for_file("bv4w_test_2.csv")
+        tournament = self.tournaments.filtered(
+            lambda t: t.name in ["event 0, tournament 0"]
+        )
+        tournament.component_ids = self.component_model.browse()
+        with self.assertRaises(ValidationError) as ue:
+            wizard.import_csv_bv4w()
+        self.assertIn(tournament.name, ue.exception.name)
+        self.assertIn("test team 2", ue.exception.name)
+        self.assertIn("TestTeamComponent2", ue.exception.name)
+
+    def test_import_csv_bv4w_components_overlap(self):
+        """
+        Import the file, check that the components are created
+        and overlapping components from different tournaments are merged.
+        """
+        wizard = self.get_wizard_for_file("bv4w_test_3.csv")
+        tournaments = self.tournaments.filtered(
+            lambda t: t.name in ["event 0, tournament 0", "event 0, tournament 1"]
+        )
+        tournaments.mapped("component_ids").unlink()
+
+        wizard.import_csv_bv4w()
+        for tournament in tournaments:
+            self.assertEqual(len(tournament.component_ids), 2)
+        components = tournaments.mapped("component_ids")
+        self.assertEqual(len(components), 3)
+
+    def test_import_csv_bv4w_components_homonym(self):
+        """
+        Import the file, check that the components are created
+        and homonyms (born in different dates) are not merged.
+        """
+        wizard = self.get_wizard_for_file("bv4w_test_4.csv")
+        tournament = self.tournaments.filtered(
+            lambda t: t.name in ["event 0, tournament 0"]
+        )
+        tournament.component_ids.unlink()
+        wizard.import_csv_bv4w()
+        self.assertEqual(len(tournament.component_ids), 4)
