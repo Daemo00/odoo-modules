89ee871de6ebd35d5ba8e80f6e36b60a7578e923
Simone Rubino <daemo00@gmail.com>
2020-08-06 23:53:46 +0200

    [ADD] event_tournament
    * Initial commit, samples from https://github.com/OCA/maintainer-quality-tools/tree/master/sample_files
    
    * models and constraints
    
    * match button_win
    
    * match with sets
    
    * match perfection
    
    * tournament, team and match fixes
    
    * fix team constrains
    
    * tournament rules
    
    * court
    
    * court constrains and various improvements
    
    * matches generation
    
    * check components in other courts
    
    * match computed fields
    
    * warm_up
    
    * try smart match assignation
    
    * smart match assignation working
    
    * randomize option
    
    * reset option
    
    * resetting only undone matches
    
    * better contemporary domain
    
    * better filter for team and match
    
    * fix tournament started state
    
    * team points ratio
    
    * refactoring team components constrains, tournament search view
    
    * computing team points
    
    * added tournament mode
    
    * added tournament mode points computation
    
    * data for match modes
    
    * refactor tournament actions
    
    * max start for team generation is last match's end
    
    * better data
    
    * time constraint on court
    
    * better points computation for matches
    
    * manage tied match with no winner
    
    * ties not allowed in sets
    
    * recompute points button in tournament
    
    * done matches can't be edited
    
    * matches schedule report
    
    * kanban view for matches
    
    * fixes by creating new tournament
    
    * posticipate end to allow scheduling a match after all matches
    
    * open tournament full-screen
    
    * very basic import wizard, no logic
    
    * parsing team lines
    
    * parsing team lines to values for team model
    
    * import succeeds
    
    * import succeeds and is correct
    
    * refactor
    
    * translations
    
    * add mobile
    
    * match schedule report
    
    * avoid playing two matches in a row
    
    * fix translation
    
    * fix sets played, match view
    
    * clean registration name
    
    * [MIG] event_tournament: Migration to 13.0
    
    * [IMP] event_tournament: Estimated match count
    
    * [IMP] event_tournament: Court check in matches generation
    
    * [IMP] event_tournament: Updated it translations
    
    * [IMP] event_tournament: More strict check on tournament name, components field in tournament, more strict check with existing registrations during import, updated it translations
    
    * [IMP] event_tournament: First tests
    
    * [IMP] event_tournament: test event_registration
    
    * [IMP] event_tournament: initial tests for event_tournament
    
    * [IMP] event_tournament: test points computation
    
    * [IMP] event_tournament: fix warning 'event_id' is readonly
    
    * [IMP] event_tournament: test match mode error, begin match generation tests
    
    * [IMP] event_tournament: more match generation tests
    
    * [IMP] event_tournament: clean common tests
    
    * [IMP] event_tournament: reformat winner computation and tests
    
    * [IMP] event_tournament: test team, better data generation for tests
    
    * [IMP] event_tournament: test tournament states
    
    * [IMP] event_tournament: improved generic tests data, tournament tests
    
    * [IMP] event_tournament: refactor common tests
    
    * [IMP] event_tournament: improved team ui and tests
    
    * [IMP] event_tournament: updated translations
    
    * [IMP] event_tournament: gender tests on teams
    
    * [IMP] event_tournament: increased event_tournament coverage
    
    * [IMP] event_tournament: increased event_tournament_team coverage
    
    * pre-commit
    
    * update quality tools configuration

---
+++ event_tournament/tests/test_event_tournament_team.py
@@ -0,0 +1,156 @@
+#  Copyright 2019 Simone Rubino <daemo00@gmail.com>
+#  License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo.exceptions import ValidationError
+from odoo.fields import first
+
+from .test_common import TestCommon
+
+
+class TestEventTournamentTeam(TestCommon):
+    def test_onchange_tournament(self):
+        """
+        Change the tournament,
+        check that domain of components and matches is modified.
+        """
+        team = first(self.teams)
+        onchange_values = team.onchange_tournament()
+        self.assertIn("domain", onchange_values)
+        self.assertIn("component_ids", onchange_values["domain"])
+        self.assertIn("match_ids", onchange_values["domain"])
+
+    def test_constrain_components_event(self):
+        """
+        Create a team,
+        check that components of a team are from the same event.
+        """
+        component_0 = first(self.events[0].registration_ids)
+        component_1 = first(self.events[1].registration_ids)
+        self.assertTrue(component_0)
+        self.assertTrue(component_1)
+        team_name = "test constrain components"
+        with self.assertRaises(ValidationError) as ve:
+            self.team_model.create(
+                {
+                    "name": team_name,
+                    "tournament_id": first(self.tournaments).id,
+                    "component_ids": [(4, component_0.id), (4, component_1.id)],
+                }
+            )
+        self.assertIn(team_name, str(ve.exception))
+
+    def test_constrain_max_components_tournament(self):
+        """
+        Create a team,
+        check that components of a team cannot be
+        more than max components set on its tournament.
+        """
+        tournament = first(self.tournaments)
+        team = first(tournament.team_ids)
+        tournament.max_components = len(team.component_ids) - 1
+        with self.assertRaises(ValidationError) as ve:
+            tournament.action_check_rules()
+
+        self.assertIn(team.name, str(ve.exception))
+        self.assertIn(tournament.name, str(ve.exception))
+
+    def test_constrain_min_components_tournament(self):
+        """
+        Create a team,
+        check that components of a team cannot be
+        less than min components set on its tournament.
+        """
+        tournament = first(self.tournaments)
+        team = first(tournament.team_ids)
+        tournament.min_components = len(team.component_ids) + 1
+        with self.assertRaises(ValidationError) as ve:
+            tournament.action_check_rules()
+
+        self.assertIn(team.name, str(ve.exception))
+        self.assertIn(tournament.name, str(ve.exception))
+
+    def test_constrain_min_components_female_tournament(self):
+        """
+        Create a team,
+        check that female components of a team cannot be
+        less than min components set on its tournament.
+        """
+        tournament = first(self.tournaments)
+        team = first(tournament.team_ids)
+        tournament.min_components_female = len(team.component_ids) - 1
+
+        # Generic gender check on teams
+        with self.assertRaises(ValidationError) as ve:
+            tournament.action_check_rules()
+
+        tournament.component_ids.update({"gender": "female"})
+        # Female check on teams succeeds
+        tournament.action_check_rules()
+
+        tournament.min_components_female = len(team.component_ids) + 1
+
+        # Female check on teams now fails
+        with self.assertRaises(ValidationError) as ve:
+            tournament.action_check_rules()
+
+        self.assertIn(team.name, str(ve.exception))
+        self.assertIn(tournament.name, str(ve.exception))
+
+    def test_constrain_min_components_male_tournament(self):
+        """
+        Create a team,
+        check that male components of a team cannot be
+        less than min components set on its tournament.
+        """
+        tournament = first(self.tournaments)
+        team = first(tournament.team_ids)
+        tournament.min_components_male = len(team.component_ids) - 1
+
+        # Generic gender check on teams
+        with self.assertRaises(ValidationError) as ve:
+            tournament.action_check_rules()
+
+        tournament.component_ids.update({"gender": "male"})
+        # Male check on teams succeeds
+        tournament.action_check_rules()
+
+        tournament.min_components_male = len(team.component_ids) + 1
+
+        # Male check on teams now fails
+        with self.assertRaises(ValidationError) as ve:
+            tournament.action_check_rules()
+
+        self.assertIn(team.name, str(ve.exception))
+        self.assertIn(tournament.name, str(ve.exception))
+
+    def test_compute_matches_points(self):
+        """
+        End a match,
+        check that points are correctly computed.
+        """
+        teams = self.teams[:2]
+        match = self.get_match_1_2(teams)
+        match.match_mode_id = self.ref(
+            "event_tournament.event_tournament_match_mode_beach_volley"
+        )
+        winner = teams[1]
+        self.assertEqual(winner.sets_won, 0)
+        self.assertEqual(winner.points_ratio, 0)
+        self.assertEqual(winner.matches_points, 0)
+        match.action_done()
+        self.assertEqual(winner.sets_won, 2)
+        self.assertEqual(winner.matches_points, 2)
+        self.assertEqual(winner.points_ratio, winner.points_done / winner.points_taken)
+
+    def test_action_view_matches(self):
+        """
+        Create a team,
+        check that `action_view_matches` creates an action
+        that shows its matches.
+        """
+        team = self.teams[:1]
+        action = team.action_view_matches()
+        action_model = action.get("res_model")
+        action_domain = action.get("domain")
+        action_matches = self.env[action_model].search(action_domain)
+        self.assertEqual(team.match_ids, action_matches)
