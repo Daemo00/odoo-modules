8b4add265e3c178972ac5967b2afcaf5688619a0
Simone Rubino <daemo00@gmail.com>
2020-08-06 23:53:46 +0200

    [ADD] event_tournament
    * Initial commit, samples from https://github.com/OCA/maintainer-quality-tools/tree/master/sample_files
    
    * models and constraints
    
    * match button_win
    
    * match with sets
    
    * match perfection
    
    * tournament, team and match fixes
    
    * fix team constrains
    
    * tournament rules
    
    * court
    
    * court constrains and various improvements
    
    * matches generation
    
    * check components in other courts
    
    * match computed fields
    
    * warm_up
    
    * try smart match assignation
    
    * smart match assignation working
    
    * randomize option
    
    * reset option
    
    * resetting only undone matches
    
    * better contemporary domain
    
    * better filter for team and match
    
    * fix tournament started state
    
    * team points ratio
    
    * refactoring team components constrains, tournament search view
    
    * computing team points
    
    * added tournament mode
    
    * added tournament mode points computation
    
    * data for match modes
    
    * refactor tournament actions
    
    * max start for team generation is last match's end
    
    * better data
    
    * time constraint on court
    
    * better points computation for matches
    
    * manage tied match with no winner
    
    * ties not allowed in sets
    
    * recompute points button in tournament
    
    * done matches can't be edited
    
    * matches schedule report
    
    * kanban view for matches
    
    * fixes by creating new tournament
    
    * posticipate end to allow scheduling a match after all matches
    
    * open tournament full-screen
    
    * very basic import wizard, no logic
    
    * parsing team lines
    
    * parsing team lines to values for team model
    
    * import succeeds
    
    * import succeeds and is correct
    
    * refactor
    
    * translations
    
    * add mobile
    
    * match schedule report
    
    * avoid playing two matches in a row
    
    * fix translation
    
    * fix sets played, match view
    
    * clean registration name
    
    * [MIG] event_tournament: Migration to 13.0
    
    * [IMP] event_tournament: Estimated match count
    
    * [IMP] event_tournament: Court check in matches generation
    
    * [IMP] event_tournament: Updated it translations
    
    * [IMP] event_tournament: More strict check on tournament name, components field in tournament, more strict check with existing registrations during import, updated it translations
    
    * [IMP] event_tournament: First tests
    
    * [IMP] event_tournament: test event_registration
    
    * [IMP] event_tournament: initial tests for event_tournament
    
    * [IMP] event_tournament: test points computation
    
    * [IMP] event_tournament: fix warning 'event_id' is readonly
    
    * [IMP] event_tournament: test match mode error, begin match generation tests
    
    * [IMP] event_tournament: more match generation tests
    
    * [IMP] event_tournament: clean common tests
    
    * [IMP] event_tournament: reformat winner computation and tests
    
    * [IMP] event_tournament: test team, better data generation for tests
    
    * [IMP] event_tournament: test tournament states
    
    * [IMP] event_tournament: improved generic tests data, tournament tests
    
    * [IMP] event_tournament: refactor common tests
    
    * [IMP] event_tournament: improved team ui and tests
    
    * [IMP] event_tournament: updated translations
    
    * [IMP] event_tournament: gender tests on teams
    
    * [IMP] event_tournament: increased event_tournament coverage
    
    * [IMP] event_tournament: increased event_tournament_team coverage
    
    * pre-commit
    
    * update quality tools configuration

---
+++ event_tournament/README.rst
@@ -0,0 +1,1 @@
+Implement tournaments in Odoo.
---
+++ event_tournament/tests/test_common.py
@@ -0,0 +1,200 @@
+#  Copyright 2019 Simone Rubino <daemo00@gmail.com>
+#  License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo.fields import first
+from odoo.tests import TransactionCase, date
+
+EVENT_NBR = 2
+REGISTRATION_NBR = 2
+TOURNAMENT_NBR = 3
+TEAM_NBR = 4
+COMPONENT_NBR = 5
+COURT_NBR = 2
+
+
+class TestCommon(TransactionCase):
+    def setUp(self):
+        super().setUp()
+        self.event_model = self.env["event.event"]
+        self.component_model = self.env["event.registration"]
+        self.court_model = self.env["event.tournament.court"]
+        self.match_model = self.env["event.tournament.match"]
+        self.match_mode_model = self.env["event.tournament.match.mode"]
+        self.team_model = self.env["event.tournament.team"]
+        self.tournament_model = self.env["event.tournament"]
+
+        self.events = self.event_model.browse()
+        self.tournaments = self.tournament_model.browse()
+        self.teams = self.team_model.browse()
+        self.registrations = self.component_model.browse()
+        self.components = self.component_model.browse()
+        self.courts = self.court_model.browse()
+
+        for event_index in range(EVENT_NBR):
+            event = self.create_event(event_index)
+
+            registrations = self.create_registrations(event, event_index)
+            self.registrations |= registrations
+
+            courts = self.create_courts(event, event_index)
+            self.courts |= courts
+
+            for tournament_index in range(TOURNAMENT_NBR):
+                tournament = self.create_tournament(
+                    courts, event, event_index, tournament_index
+                )
+                self.tournaments |= tournament
+                for team_index in range(TEAM_NBR):
+                    team = self.create_team(
+                        event_index, team_index, tournament, tournament_index
+                    )
+                    self.teams |= team
+                    for component_index in range(COMPONENT_NBR):
+                        component = self.create_component(
+                            component_index,
+                            event,
+                            event_index,
+                            team,
+                            team_index,
+                            tournament_index,
+                        )
+                        self.components |= component
+
+    def create_component(
+        self, component_index, event, event_index, team, team_index, tournament_index
+    ):
+        component = self.component_model.create(
+            {
+                "event_id": event.id,
+                "tournament_team_ids": [(4, team.id)],
+                "name": "event {event_index}, "
+                "tournament {tournament_index}, "
+                "team {team_index}, "
+                "component {component_index}".format(
+                    event_index=event_index,
+                    tournament_index=tournament_index,
+                    team_index=team_index,
+                    component_index=component_index,
+                ),
+            }
+        )
+        return component
+
+    def create_team(self, event_index, team_index, tournament, tournament_index):
+        team = self.team_model.create(
+            {
+                "tournament_id": tournament.id,
+                "name": "event {event_index}, "
+                "tournament {tournament_index}, "
+                "team {team_index}".format(
+                    event_index=event_index,
+                    tournament_index=tournament_index,
+                    team_index=team_index,
+                ),
+            }
+        )
+        return team
+
+    def create_tournament(self, courts, event, event_index, tournament_index):
+        tournament = self.tournament_model.create(
+            {
+                "event_id": event.id,
+                "court_ids": courts.ids,
+                "name": "event {event_index}, "
+                "tournament {tournament_index}".format(
+                    event_index=event_index, tournament_index=tournament_index
+                ),
+            }
+        )
+        return tournament
+
+    def create_courts(self, event, event_index):
+        courts = self.court_model.browse()
+        for court_index in range(COURT_NBR):
+            courts |= self.court_model.create(
+                {
+                    "event_id": event.id,
+                    "name": "event {event_index}, "
+                    "court {court_index}".format(
+                        event_index=event_index, court_index=court_index
+                    ),
+                }
+            )
+        return courts
+
+    def create_registrations(self, event, event_index):
+        registrations = self.component_model.browse()
+        for registration_index in range(REGISTRATION_NBR):
+            registrations |= self.component_model.create(
+                {
+                    "event_id": event.id,
+                    "name": "event {event_index}, "
+                    "component {registration_index}".format(
+                        event_index=event_index, registration_index=registration_index
+                    ),
+                }
+            )
+        return registrations
+
+    def create_event(self, event_index):
+        event = self.event_model.create(
+            {
+                "name": "event {event_index}".format(event_index=event_index),
+                "date_begin": date(2000, 1, 1),
+                "date_end": date(2000, 1, 2),
+            }
+        )
+        self.events |= event
+        return event
+
+    def get_match_1_2(self, teams):
+        tournament = first(self.tournaments)
+        court = first(self.courts)
+        tournament.update({"court_ids": court.ids})
+        teams.update({"tournament_id": tournament.id})
+        match_values = {
+            "tournament_id": tournament.id,
+            "court_id": court.id,
+            "team_ids": teams.ids,
+        }
+        match_values.update(self.get_match_lines_1_2(teams))
+        match = self.match_model.create(match_values)
+        return match
+
+    def get_match_lines_1_2(self, teams):
+        line_1_values = {
+            "team_id": teams[0].id,
+            "set_1": 10,
+            "set_3": 21,
+            "set_4": 8,
+        }
+        line_2_values = {
+            "team_id": teams[1].id,
+            "set_1": 21,
+            "set_3": 18,
+            "set_4": 15,
+        }
+        return {
+            "line_ids": [(0, 0, line_1_values), (0, 0, line_2_values)],
+        }
+
+    def get_match_1_1(self, teams):
+        tournament = first(self.tournaments)
+        court = first(self.courts)
+        tournament.update({"court_ids": court.ids})
+        teams.update({"tournament_id": tournament.id})
+        match_values = {
+            "tournament_id": tournament.id,
+            "court_id": court.id,
+            "team_ids": teams.ids,
+        }
+        match_values.update(self.get_match_lines_1_1(teams))
+        match = self.match_model.create(match_values)
+        return match
+
+    def get_match_lines_1_1(self, teams):
+        return {
+            "line_ids": [
+                (0, 0, {"team_id": teams[0].id, "set_1": 10, "set_3": 21}),
+                (0, 0, {"team_id": teams[1].id, "set_1": 21, "set_3": 18}),
+            ],
+        }
