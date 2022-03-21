ac06dc8286ec92bf921cac40f52dfadb3386513c
Simone Rubino <daemo00@gmail.com>
2022-03-21 23:55:00 +0100

    [MIG] event_tournament: Migration to 15.0

--- event_tournament/__manifest__.py
+++ event_tournament/__manifest__.py
@@ -4,9 +4,10 @@
 {
     "name": "Event tournament",
     "summary": "Implement tournaments in Odoo events",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Simone Rubino, Odoo Community Association (OCA)",
+    "author": "Daemo00's developments",
+    "website": "https://github.com/Daemo00/odoo-modules/tree/15.0/event_tournament",
     "depends": ["event", "partner_contact_birthdate", "partner_contact_gender"],
     "data": [
         "security/ir.model.access.csv",
