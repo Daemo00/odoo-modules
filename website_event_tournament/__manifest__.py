82cfb5890077cf8c586b237e9091903e036378ef
Simone Rubino <daemo00@gmail.com>
2022-03-25 23:01:10 +0100

    Website integration

---
+++ website_event_tournament/__manifest__.py
@@ -0,0 +1,18 @@
+#  Copyright 2022 Simone Rubino
+#  License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Tournaments in website",
+    "summary": "Manage tournaments in website",
+    "version": "15.0.1.0.0",
+    "category": "Website",
+    "author": "Simone Rubino, Odoo Community Association (OCA)",
+    "website": "https://github.com/Daemo00/odoo-modules/tree/15.0/event_tournament",
+    "license": "AGPL-3",
+    "depends": [
+        "website_event",
+        "event_tournament",
+    ],
+    "data": [
+        "templates/event_registration_templates.xml",
+    ],
+}
