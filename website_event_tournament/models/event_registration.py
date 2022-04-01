6a2ba136e58a5410535c99d43e61c10c54bc9fe4
Simone Rubino <daemo00@gmail.com>
2022-04-01 20:26:35 +0200

    Common registration fields in website, missing date parsing

---
+++ website_event_tournament/models/event_registration.py
@@ -0,0 +1,20 @@
+#  Copyright 2022 Simone Rubino
+#  License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class EventRegistration(models.Model):
+    _inherit = "event.registration"
+
+    def _get_website_registration_allowed_fields(self):
+        allowed_fields = super()._get_website_registration_allowed_fields()
+        allowed_fields.update(
+            {
+                "birthday_date",
+                "gender",
+                "mobile",
+                "is_fipav",
+            }
+        )
+        return allowed_fields
