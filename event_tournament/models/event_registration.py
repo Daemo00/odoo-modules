cbddf04aec4487b62f5b0325ec1e80cbd64a2f9f
Simone Rubino <daemo00@gmail.com>
2022-05-15 19:45:36 +0200

    [IMP] event_tournament: Points for each component

--- event_tournament/models/event_registration.py
+++ event_tournament/models/event_registration.py
@@ -14,6 +14,12 @@ class EventRegistration(models.Model):
         column1="team_id",
         column2="component_id",
     )
+    points_done = fields.Float(compute="_compute_teams_points", store=True)
+    points_taken = fields.Float(compute="_compute_teams_points", store=True)
+    points_ratio = fields.Float(compute="_compute_teams_points", store=True)
+    sets_won = fields.Integer(compute="_compute_teams_points", store=True)
+    matches_points = fields.Integer(compute="_compute_teams_points", store=True)
+    matches_done = fields.Integer(compute="_compute_teams_points", store=True)
     tournament_ids = fields.Many2many(
         comodel_name="event.tournament", compute="_compute_tournaments", store=True
     )
@@ -36,9 +42,33 @@ class EventRegistration(models.Model):
                 self.mobile = contact.mobile or self.mobile
         return res
 
-    @api.depends("tournament_team_ids")
+    @api.depends(
+        "tournament_team_ids.tournament_id",
+    )
     def _compute_tournaments(self):
         for registration in self:
             registration.tournament_ids = registration.tournament_team_ids.mapped(
                 "tournament_id"
             )
+
+    @api.depends(
+        "tournament_team_ids.points_done",
+        "tournament_team_ids.points_taken",
+        "tournament_team_ids.points_ratio",
+        "tournament_team_ids.sets_won",
+        "tournament_team_ids.matches_points",
+        "tournament_team_ids.match_ids.state",
+    )
+    def _compute_teams_points(self):
+        for registration in self:
+            teams = registration.tournament_team_ids
+            registration.points_done = sum(team.points_done for team in teams)
+            registration.points_taken = sum(team.points_taken for team in teams)
+            registration.points_ratio = registration.points_done / (
+                registration.points_taken or 1
+            )
+            registration.sets_won = sum(team.sets_won for team in teams)
+            registration.matches_points = sum(team.matches_points for team in teams)
+
+            matches = teams.match_ids.filtered(lambda m: m.state == "done")
+            registration.matches_done = len(matches)
