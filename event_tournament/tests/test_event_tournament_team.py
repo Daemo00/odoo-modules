3d0b4c54b453055f702ebbeb6263f6aa974520ee
Simone Rubino <daemo00@gmail.com>
2023-08-03 22:40:32 +0200

    [REF] event_tournament: Update Copyright

--- event_tournament/tests/test_event_tournament_team.py
+++ event_tournament/tests/test_event_tournament_team.py
@@ -1,4 +1,4 @@
-#  Copyright 2020 Simone Rubino <daemo00@gmail.com>
+#  Copyright 2020 ~ 2023 Simone Rubino <daemo00@gmail.com>
 #  License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 import psycopg2
 
